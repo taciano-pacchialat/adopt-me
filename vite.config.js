928b8f8d349b5e72a754c2df31f3846419766718
Taciano <taciano3003@gmail.com>
2023-09-03 17:33:55 -0300

    solved typo regarding vite.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugins-react";
+import react from "@vitejs/plugin-react";
 
-export default defaultConfig({
+export default defineConfig({
   plugins: [react()],
   root: "src",
 });
