6ac1847bb8d1699affabba308ea8cdb671e1faaf
Taciano <taciano3003@gmail.com>
2023-09-02 22:33:14 -0300

    Lesson 2 cleared Creating components with arguments

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement(
+    "div", // kind of element we're putting
+    {}, // atributes -> id, className, etc
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet, {
+        animal: "Dog",
+        name: "Luna",
+        breed: "Havanese",
+      }),
+      React.createElement(
+        Pet,
+        React.createElement(Pet, {
+          animal: "Cat",
+          name: "Doink",
+          breed: "Mixed",
+        })
+      ),
+      React.createElement(
+        Pet,
+        React.createElement(Pet, {
+          animal: "Bird",
+          name: "Pepper",
+          breed: "Cockatiel",
+        })
+      ),
+    ]
+  );
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
