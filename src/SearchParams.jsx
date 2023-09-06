587ed78837c3ce7bfbeddd061234932d1fa304b8
Taciano <taciano3003@gmail.com>
2023-09-06 19:24:40 -0300

    added animal breed into the form.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -4,6 +4,9 @@ const ANIMALS = ["birds", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState(" ");
   const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
@@ -16,12 +19,16 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+
         <label htmlFor="animal">
           Animal
           <select
             id="animal"
             value={animal}
-            onChange={(e) => setAnimal(e.target.value)}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
           >
             <option />
             {ANIMALS.map((animal) => (
@@ -29,6 +36,22 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={breeds.length === 0}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
