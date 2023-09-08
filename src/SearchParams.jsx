acb7db67e105004c0141d9019adbf27d680a620c
Taciano <taciano3003@gmail.com>
2023-09-08 18:37:20 -0300

    component composition lesson finished.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import Pet from "./Pet";
 import useBreedList from "./useBreedList";
+import Results from "./Results";
+
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -11,6 +12,7 @@ const SearchParams = () => {
   const [breeds] = useBreedList(animal);
 
   useEffect(() => {
+    // runs once
     requestPets();
   }, []);
 
@@ -74,17 +76,9 @@ const SearchParams = () => {
             ))}
           </select>
         </label>
-
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
