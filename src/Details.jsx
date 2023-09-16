68d766a99c3bd11ea50daba1b8b33596b5c4a9c8
Taciano <taciano3003@gmail.com>
2023-09-16 19:12:51 -0300

    context lesson finished.

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,8 +3,11 @@ import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPets";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import { useState } from "react";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -29,8 +32,19 @@ const Details = () => {
         <h1>{pet.name}</h1>
         <h2>
           {pet.animal}, {pet.breed}, {pet.city}, {pet.state}
-          <button>Adopt {pet.name}</button>
+          <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
           <p>{pet.description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}?</h1>
+                <div className="buttons">
+                  <button>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </h2>
       </div>
     </div>
